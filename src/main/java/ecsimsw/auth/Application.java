03bc10212308394523bd19a42cf708ed7721a4f3
ecsimsw <ecsimsw@gmail.com>
2023-11-22 18:44:19 +0900

    remove sample files

--- src/main/java/ecsimsw/auth/Application.java
+++ src/main/java/ecsimsw/auth/Application.java
@@ -3,11 +3,8 @@ package ecsimsw.auth;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class SampleApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication application = new SpringApplication(SampleApplication.class);
-        application.run(args);
     }
 }
