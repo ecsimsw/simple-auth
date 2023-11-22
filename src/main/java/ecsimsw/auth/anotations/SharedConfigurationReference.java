5a4d5c3ed1a6b07021fca200cafc58b949d70664
ecsimsw <ecsimsw@gmail.com>
2023-11-23 05:56:20 +0900

    add EnableRedisRepositories

--- src/main/java/ecsimsw/auth/anotations/SharedConfigurationReference.java
+++ src/main/java/ecsimsw/auth/anotations/SharedConfigurationReference.java
@@ -3,8 +3,10 @@ package ecsimsw.auth.anotations;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
 @Configuration
-@ComponentScan("ecsimsw.auth")
-@EntityScan("ecsimsw.auth")
+@ComponentScan(basePackages = "ecsimsw.auth")
+@EntityScan(basePackages = "ecsimsw.auth")
+@EnableRedisRepositories(basePackages = "ecsimsw.auth")
 public class SharedConfigurationReference {}
