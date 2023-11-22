9222b5ad7a3bce8bc5aa559e8bfaf9724ff1e6fe
ecsimsw <ecsimsw@gmail.com>
2023-11-23 05:51:18 +0900

    add test component

---
+++ src/main/java/ecsimsw/auth/service/SampleComponent.java
@@ -0,0 +1,11 @@
+package ecsimsw.auth.service;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class SampleComponent {
+
+    public void hi() {
+        System.out.println("hi");
+    }
+}
