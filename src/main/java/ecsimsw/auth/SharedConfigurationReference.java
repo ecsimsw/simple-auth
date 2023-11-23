121369a806b11a04687edbcee21ec9977ac4891e
ecsimsw <ecsimsw@gmail.com>
2023-11-23 21:26:40 +0900

    remove entity scan

--- src/main/java/ecsimsw/auth/SharedConfigurationReference.java
+++ src/main/java/ecsimsw/auth/SharedConfigurationReference.java
@@ -1,13 +1,11 @@
 package ecsimsw.auth;
 
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
 @Configuration
 @ComponentScan(basePackages = "ecsimsw.auth")
-@EntityScan(basePackages = "ecsimsw.auth")
 @EnableRedisRepositories(basePackages = "ecsimsw.auth")
 public class SharedConfigurationReference {
 
