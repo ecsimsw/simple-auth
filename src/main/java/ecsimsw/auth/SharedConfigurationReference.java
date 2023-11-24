38280bb5031c530f64f9d6de615ee73839f6d505
ecsimsw <ecsimsw@gmail.com>
2023-11-24 12:56:26 +0900

    remove autoconfiguration

--- src/main/java/ecsimsw/auth/SharedConfigurationReference.java
+++ src/main/java/ecsimsw/auth/SharedConfigurationReference.java
@@ -1,11 +1,12 @@
 package ecsimsw.auth;
 
-import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
-@AutoConfiguration
+@Configuration
+@ComponentScan(basePackages = "ecsimsw.auth")
+@EnableRedisRepositories(basePackages = "ecsimsw.auth")
 public class SharedConfigurationReference {
 
     public static void main(String[] args) {
